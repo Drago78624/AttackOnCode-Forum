19a8e68140b45d90cf57e3d03ab909bcb9efde55
Muhammad Maaz Ahmed <maazahmed78624@gmail.com>
2022-07-06 08:42:48 +0500

    about pg added

--- about.php
+++ about.php
@@ -1,1 +1,33 @@
-about
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/about.css">
+    <title>AttackOnCode - About</title>
+</head>
+
+<body>
+    <?php include "./partials/_navbar.php" ?>
+    <main class="about-section">
+        <div class="about-container">
+            <h1 class="about-heading">About</h1>
+            <div class="about">
+                <p>AttackOnCode was created in 2022 by <strong> Muhammad Maaz Ahmed</strong>. The whole purpose of this website is, to provide newbie developers a free will to ask
+questions related to programming and their coding issues. There are many websites on the internet available for developers to ask
+questions but I felt that there is a need for a new platform for newbie developers where they can ask questions freely without the fear
+of being downvoted. Every programming related question is appreciated here, but be careful not to violate any of the <a href="terms-conditions.php" class="tc">terms and conditions</a>. Enjoy and Attack each other with your queries!</p>
+            </div>
+        </div>
+    </main>
+    <?php include "./partials/_footer.php" ?>
+
+
+    <script src="./script/app.js"></script>
+</body>
+
+</html>
